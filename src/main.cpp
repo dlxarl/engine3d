c848b4a95dafea3376df66435b614cf0c0fa85c9
Illia Zinchuk <zinchukillia@gmail.com>
2025-11-27 14:14:49 +0100

    Mouse and keyboard handling

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,73 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+float yaw   = -90.0f;
+float pitch =  0.0f;
+float fov   =  45.0f;
+
+float lastX = SCR_WIDTH / 2.0;
+float lastY = SCR_HEIGHT / 2.0;
+bool firstMouse = true;
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn) {
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    std::cout << "Mouse X: " << xpos << " Y: " << ypos << std::endl; // pos
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+    fov -= (float)yoffset;
+    if (fov < 1.0f)
+        fov = 1.0f;
+    if (fov > 45.0f)
+        fov = 45.0f;
+
+    std::cout << "Zoom (FOV): " << fov << std::endl;
+}
+
 void processInput(GLFWwindow *window) {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        std::cout << "Going Forward!" << std::endl;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        std::cout << "Going Backward!" << std::endl;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        std::cout << "Strafe Left!" << std::endl;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        std::cout << "Strafe Right!" << std::endl;
 }
 
 int main() {
@@ -21,14 +81,19 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "3D Engine", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "3D Engine", NULL, NULL);
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
+
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
@@ -37,8 +102,8 @@ int main() {
 
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
-
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        float greenValue = (pitch + 90.0f) / 180.0f;
+        glClearColor(0.2f, greenValue, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glfwSwapBuffers(window);
